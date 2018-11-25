5a0c73d8984b4b4be7aaf18b850506db3204b15f
felipe-clark <felipe.clark@gmail.com>
2018-11-25 18:23:06 -0500

    PDF report export completed

--- app/issuesmain.py
+++ app/issuesmain.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, session
 from app import IssueTracker
 from app.tools.dbTools import DataBaseManager
+from app.tools.pdfTools import PdfGenerator
 
 @IssueTracker.route('/main_issue_list')
 def render_main_issue_list():
@@ -8,5 +9,13 @@ def render_main_issue_list():
         issues = DataBaseManager.get_issues()
 
         return render_template("mainissuelist.html")
+    return redirect(url_for("index"))
 
+@IssueTracker.route('/export_to_pdf', methods=['POST'])
+def export_to_pdf():
+    if 'authorized' in session and session['authorized'] is True:
+        issues = DataBaseManager.get_issues()
+        pdf = PdfGenerator.format_pdf(issues)
+
+        return PdfGenerator.create_pdf_file(pdf)
     return redirect(url_for("index"))
---
+++ app/tools/pdfTools.py
@@ -0,0 +1,42 @@
+from fpdf import FPDF
+from flask import make_response
+
+
+class PdfGenerator:
+    @staticmethod
+    def create_pdf_file(pdf):
+        response = make_response(pdf.output(dest='S').encode('latin-1'))
+        response.headers.set('Content-Disposition', 'attachment', filename='Report.pdf')
+        response.headers.set('Content-Type', 'application/pdf')
+
+        return response
+
+    @staticmethod
+    def format_pdf(data):
+        pdf = FPDF(format='A4', unit='in')
+        pdf.add_page()
+
+        effective_page_width = pdf.w - 2 * pdf.l_margin
+
+        for item in data:
+            pdf.set_font('Times', 'B', 15.0)
+            pdf.cell(effective_page_width, 0.0, 'Project: {}'.format(item.get('project')), align='C')
+            pdf.ln(0.6)
+            break
+
+        for item in data:
+            pdf.set_font('Times', 'B', 10.0)
+            pdf.cell(1.0, 0.0, 'ID: {} - Logged in: {}'.format(item.get('id'), item.get('date_added')))
+            pdf.ln(0.15)
+            pdf.cell(1.0, 0.0, 'Document: {}'.format(item.get('document')))
+            pdf.ln(0.15)
+            pdf.cell(1.0, 0.0, 'Discipline: {}'.format(item.get('discipline')))
+            pdf.ln(0.15)
+            pdf.cell(1.0, 0.0, 'Status: {}'.format(item.get('status')))
+            pdf.ln(0.25)
+
+            pdf.set_font('Times', '', 10.0)
+            pdf.multi_cell(effective_page_width, 0.15, item.get('description'))
+            pdf.ln(0.5)
+
+        return pdf
