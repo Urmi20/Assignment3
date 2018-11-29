4ca431bef2a521f8c0a076b08a9fd935756f6b19
felipe-clark <felipe.clark@gmail.com>
2018-11-28 21:29:42 -0500

    Convert ALL to None in filters

--- app/issuesmain.py
+++ app/issuesmain.py
@@ -5,14 +5,14 @@ from app.tools.pdfTools import PdfGenerator
 from app.tools.pagination import Pagination
 
 
-@IssueTracker.route('/main_issue_list')
+@IssueTracker.route('/main_issue_list', methods=['POST', 'GET'])
 def render_main_issue_list():
     if 'authorized' in session and session['authorized'] is True:
-        status_input = request.form.get('status')
-        project_input = request.form.get('project')
-        discipline_input = request.form.get('discipline')
-        sentiment_input = request.form.get('sentiment')
-        # document_input = request.form.get('document')
+        status_input = convert_all_to_none(request.form.get('status'))
+        project_input = convert_all_to_none(request.form.get('project'))
+        discipline_input = convert_all_to_none(request.form.get('discipline'))
+        sentiment_input = convert_all_to_none(request.form.get('sentiment'))
+        # document_input = convert_all_to_none(request.form.get('document'))
 
         issues, last_evaluated_key = Pagination.page_data(None, project_input, None, discipline_input, sentiment_input, status_input)
         projects = DataBaseManager.get_projects()
@@ -32,3 +32,10 @@ def export_to_pdf():
 
         return PdfGenerator.create_pdf_file(pdf)
     return redirect(url_for("index"))
+
+
+def convert_all_to_none(text):
+    if text == "All":
+        return None
+    else:
+        return text
