51e5cec4f01f999fcb3c4fd14e51a0e441330a81
felipe-clark <felipe.clark@gmail.com>
2018-12-04 23:06:26 -0500

    Add voice synthesizer functions

--- app/tools/dbTools.py
+++ app/tools/dbTools.py
@@ -232,7 +232,7 @@ class DataBaseManager:
         return disciplines
 
     @staticmethod
-    def add_issue(project, document, discipline,issue, date_time, id, sentiment):
+    def add_issue(project, document, discipline,issue, date_time, id, sentiment, voice):
         dynamodb = boto3.resource('dynamodb')
         table_name = 'it_issues'
 
@@ -248,7 +248,8 @@ class DataBaseManager:
                     'date_added': date_time,
                     'discipline': discipline,
                     'description': issue,
-                    'sentiment': sentiment
+                    'sentiment': sentiment,
+                    'voice': voice
                 },
                 ConditionExpression='attribute_not_exists(id)'
             )
